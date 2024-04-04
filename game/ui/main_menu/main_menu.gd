859507a4c22d27af396229a7931d2c7bb19eba2d
Vilij-Corps <141726065+vilij-corps@users.noreply.github.com>
2024-04-04 16:19:27 +0300

    window resizing

--- game/ui/main_menu/main_menu.gd
+++ game/ui/main_menu/main_menu.gd
@@ -8,12 +8,16 @@
 
 extends Control
 
-@onready var start_btn = $Start_Btn
-@onready var exit_btn = $Exit_Btn
+@onready var start_btn = $menu/Start_Btn
+@onready var options_btn = $menu/Options_Btn
+@onready var progress_btn = $menu/Progress_Btn
+@onready var exit_btn = $menu/Exit_Btn
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	start_btn.button_down.connect(start_pressed)
+	options_btn.button_down.connect(options_pressed)
+	progress_btn.button_down.connect(progress_pressed)
 	exit_btn.button_down.connect(exit_pressed)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -24,6 +28,14 @@ func start_pressed():
 	print("Start Pressed")
 	ProgressManager.load_progress()
 	get_tree().change_scene_to_file("res://ui/maps/map_level_one.tscn")
+
+func options_pressed():
+	print("Options Pressed")
+	get_tree().change_scene_to_file("res://ui/resolution/sizing.tscn")
+	
+func progress_pressed():
+	print("Progress Pressed")
+	# get_tree().change_scene_to_file("res://ui/resolution/sizing.tscn")
 
 func exit_pressed():
 	print("Exit Pressed")
