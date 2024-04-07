093b69bbf5ebe7f18fb2ea50414ad3d086a755a4
Crested-Crane <141726065+vilij-corps@users.noreply.github.com>
2024-04-07 22:08:03 +0300

    nightly

--- game/scenes/cooking/_helpers/recipe_maker.gd
+++ game/scenes/cooking/_helpers/recipe_maker.gd
@@ -1,5 +1,8 @@
-extends Node2D
+extends Control
 
+var description : String = ""
+var ingredients : Array = []
+var directions : Array = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,3 +12,20 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func load_recipe_file(load_path : String):
+	if FileAccess.file_exists(load_path):
+		print("file found")
+		var file = FileAccess.open(load_path, FileAccess.READ)
+		var recipe = JSON.parse_string(file.get_as_text())
+		if recipe is Array:
+			print("success reading file")
+			description = recipe["description"]
+			ingredients = recipe["ingredients"]
+			directions = recipe["directions"]
+			return recipe
+		else:
+			print("error reading file")
+	else:
+		print("file not found")
