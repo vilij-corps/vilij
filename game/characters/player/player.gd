08fe60dec2107cc5d256c6337f915a247b0c133b
Vilij-Corps <141726065+vilij-corps@users.noreply.github.com>
2024-04-11 14:42:49 +0300

    interaction

--- game/characters/player/player.gd
+++ game/characters/player/player.gd
@@ -9,11 +9,13 @@
 extends CharacterBody2D
 
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var all_interactions = []
+@onready var interact_label_display = %InteractLabel
 
 const SPEED = 300.0
 
 func _ready():
-	pass
+	update_interactions()
 
 func _process(delta):
 	pass
@@ -29,6 +31,9 @@ func _physics_process(delta):
 
 	move_and_slide()
 	update_animations(direction)
+	
+	if Input.is_action_just_pressed("interact"):
+		execute_interaction()
 
 func update_animations(direction) -> void:
 	
@@ -39,10 +44,28 @@ func update_animations(direction) -> void:
 	elif direction == -1:
 		animated_sprite.play("walk_left")	
 
-func _unhandled_input(event):
-	if event.is_action_pressed("talk"):
-		pass
-
 func get_current_position():
-	print(self.position)
-	return self.position
+	var player_position = get_position()
+	return player_position
+
+
+func _on_interaction_area_area_entered(area):
+	all_interactions.insert(0, area)
+	update_interactions()
+
+
+func _on_interaction_area_area_exited(area):
+	all_interactions.erase(area)
+	update_interactions()
+
+func update_interactions():
+	if all_interactions:
+		interact_label_display.text = all_interactions[0].interact_label
+	else:
+		interact_label_display.text = ""
+
+func execute_interaction():
+	if all_interactions:
+			var cur_interaction = all_interactions[0]
+			match cur_interaction.interact_type:
+				"show_dialogue" : print(cur_interaction.interact_value)
---
+++ game/components/interaction/area.gd
@@ -0,0 +1,14 @@
+'''
+ +-+-+ +-+-+ +-+-+-+
+		vilij
+ +-+-+ +-+-+ +-+-+-+
+	  CC BY 4.0
+ +-+-+ +-+-+ +-+-+-+
+'''
+
+extends Area2D
+class_name Interactable
+
+@export var interact_label = "none"
+@export var interact_type = "none"
+@export var interact_value = "none"
