216846d7a0df507385eedaf292e8f843728354c3
JBatUN <jaybellone@hotmail.com>
2024-05-23 23:24:55 +0300

    header

--- docs-dev/views/graph/components/header.js
+++ docs-dev/views/graph/components/header.js
@@ -10,10 +10,11 @@ class Header extends HTMLElement {
         this.innerHTML = `
         <div class="header">
           <div class="topnav">
-              <img src="./assets/logo/vilijcorps.svg" alt="Vilij Corps" class="header-logo">
               <img src="./assets/icons/home.svg" alt="Home" class="nav-logo">
               <img src="./assets/icons/gallery.svg" alt="Gallery" class="nav-logo">
+              <img src="./assets/logo/vilijcorps.svg" alt="Vilij Corps" class="header-logo">
               <img src="./assets/icons/translator_alt.svg" alt="Translator" class="nav-logo">
+              <img src="./assets/icons/github.svg" alt="Github" class="nav-logo">
           </div>
         </div>
       `;
