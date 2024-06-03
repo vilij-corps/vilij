8f4e905b947de31e7cb3a5ffdd7d680292eaedc5
Vilij-Corps <141726065+vilij-corps@users.noreply.github.com>
2024-06-03 15:42:31 +0300

    translator

--- docs-dev/views/translator/main.js
+++ docs-dev/views/translator/main.js
@@ -13,13 +13,10 @@ var element = null;
 // handle words that don't have
 // a pictogram file yet
 function pictogram_exists(image_url){
-
     var http = new XMLHttpRequest();
     http.open('HEAD', image_url, false);
     http.send();
-
     return http.status != 404;
-
 }
 
 // render results of translation to
@@ -44,12 +41,14 @@ var render_translation = function() {
                 let checked = null;
                 // console.log(check)
 
+                // If there is no pictogram add a placeholder
                 if (check) {
                     checked = '<img src="' + terms[t].pictogram + '" style="height:48px;">'
                 } else {
                     checked = '<h4 style="margin-top:36px;">-</h4>'
                 }
                 
+                // Now add the source and translation terms
                 if (terms[t].text != terms[t].translation) {
                         card += checked
                         card += '<h4><b>' + terms[t].text + '</b></h4><div class="src_word"></div><p>' + terms[t].translation + '</p></div></div>'
@@ -57,6 +56,7 @@ var render_translation = function() {
                         card += '<h4><b>' + terms[t].text + '</b></h4><p></p></div></div>'
                 }
 
+                // the PoS result
                 card += '<div class="badge">' + terms[t].chunk + '</div>';
 
                 let card_word = document.createElement("div");
